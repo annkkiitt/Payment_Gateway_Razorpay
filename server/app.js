ddeff7c4bde3e7e22d17f1e5cacb6814f7ab408f
Ankit Rawat <ankit2001rawat@gmail.com>
2023-04-29 19:02:23 +0530

    frontend_start

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import { config } from 'dotenv';
 import paymentRoute from './routes/paymentRoutes.js';
 
 config({ path: './config/config.env' });
-
 export const app = express();
 
+app.use(cors());
 app.use('/api', paymentRoute);
